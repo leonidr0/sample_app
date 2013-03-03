b1e1678db5e697182728989719e69c72e3794b91
Leonid Rozenberg <leonidr@gmail.com>
2013-03-03 11:19:11 +0100

    Guard and spork this stuff

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  let (:base_title) { "Ruby on Rails Tutorial Sample App | " }
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,7 +13,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | Home")
+                                :text => "#{base_title}Home")
     end
 
   end
@@ -26,7 +27,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | Help")
+                                :text => "#{base_title}Help")
     end
 
 
@@ -41,7 +42,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | About Us")
+                                :text => "#{base_title}About Us")
     end
 
   end
@@ -55,7 +56,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/contact'
       page.should have_selector('title', 
-                                :text => "Ruby on Rails Tutorial Sample App | Contact")
+                                :text => "#{base_title}Contact")
     end
 
 
